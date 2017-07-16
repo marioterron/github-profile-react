4c29008b6f5d2088bcbdb836d8d8eaad008b9cf6
Mario Terron <marioterron157@gmail.com>
2017-07-17 01:51:25 +0200

    modified webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,30 +17,14 @@ module.exports = {
 
   module: {
     loaders: [
-      { 
+      {
         test: /(\.js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel' 
+        loader: 'babel'
       },
-      { 
+      {
         test: /\.css$/,
-        loader: `style-loader!css-loader?${cssModules}` 
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.(woff|woff2)$/,
-        loader: 'url-loader?prefix=font/&limit=5000'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        loader: `style-loader!css-loader?${cssModules}`
       }
     ]
   },
